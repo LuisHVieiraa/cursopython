4908a678bd6f93d581b235467941d78cbc77dab0
LuisHVieiraa <118404710+LuisHVieiraa@users.noreply.github.com>
2025-01-03 15:10:03 -0300

    teste

--- ex002.py
+++
@@ -1,7 +0,0 @@
-nome=input('Qual é o seu nome?')
-dia=input('Qual é o Dia em que você nasceu?')
-mes=input('Qual é o Mês em que você nasceu?')
-ano=input('Qual é o Ano em que você nasceu?')
-
-print('Olá,',nome,'!')
-print('Você nasceu no dia ',dia,'de',mes,'de',ano,'. Correto?')
--- ex003.py
+++
@@ -1,8 +0,0 @@
-num1=input('Digite um número')
-num2=input('Digite outro número')
-
-num1=int(num1)
-num2=int(num2)
-
-soma=num1+num2
-print('A soma de ',num1,'+',num2,'è:',soma)
